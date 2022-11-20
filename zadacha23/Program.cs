8bbfcf5c8e19509ad8a97c0576ba925ddc17d9ea
Елена <lenagolubeva96@gmail.com>
2022-11-20 23:52:43 +0200

    zadacha 23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+void CubeTable(int n)
+{
+    for (int index = 1; index <= n; index++)
+    {
+        Console.WriteLine(index + " в кубе равно " + index * index * index);
+    }
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeTable(number);
